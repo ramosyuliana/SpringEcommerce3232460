707146b98a7063c922d6cb2c664364ad4d8d1e5d
ramosyuliana <ramosyuliana288@gmail.com>
2025-11-07 13:54:12 -0500

    v0.3_creacionInterfacesPaqueteRepository_yuliana_07112025

---
+++ src/main/java/com/sena/springecommerce/repository/IUsuarioReposity.java
@@ -0,0 +1,14 @@
+package com.sena.springecommerce.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sena.springecommerce.model.Usuario;
+
+@Repository
+public interface IUsuarioReposity extends JpaRepository<Usuario, Integer> {
+	
+	Optional<Usuario> findByEmail(String email);
+}
